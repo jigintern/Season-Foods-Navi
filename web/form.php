4ce8c2741b35285f3ec5e704267ff51423c2d308
FukeKazki <kazkichi0906@gmail.com>
2019-08-27 17:17:57 +0900

    Add: 基本的なUIの作成 データベースへの挿入機能の作成

---
+++ web/form.php
@@ -0,0 +1,39 @@
+<?php
+require_once('./class.php');
+function h($str) {
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+function br() {
+    echo '<br/>';
+}
+// 食材
+if(isset($_POST['submit_1'])) {
+    $db = new DB;
+    $name = (string)filter_input(INPUT_POST, 'name');
+    $image = (string)filter_input(INPUT_POST, 'image');
+    $image_path = (string)filter_input(INPUT_POST, 'image_path');
+    $prefecture = (string)filter_input(INPUT_POST, 'prefecture');
+    $sql = 'INSERT INTO local_foods (name, picture, pref_id) VALUES (:name, :image_path, :prefecture)';
+    $result = $db->prepare($sql);
+    $result -> bindParam(':name',$name,PDO::PARAM_STR);
+    $result -> bindParam(':image_path',$image_path,PDO::PARAM_STR);
+    $result -> bindParam(':prefecture',$prefecture,PDO::PARAM_INT);
+    $result -> execute();
+}
+// 献立
+if(isset($_POST['submit_2'])) {
+    $name = (string)filter_input(INPUT_POST, 'name');
+    $image = (string)filter_input(INPUT_POST, 'image');
+    $image_path = (string)filter_input(INPUT_POST, 'image_path');
+    $prefecture = (string)filter_input(INPUT_POST, 'prefecture');
+    // とりあえずfoodリスト
+    $food = "['なす', 'とまと', 'わらび餅']";
+    $db = new DB;
+    $sql = 'INSERT INTO recipes (name, picture, foods, pref_id) VALUES (:name, :image_path, :foods, :prefecture)';
+    $result = $db->prepare($sql);
+    $result -> bindParam(':name',$name,PDO::PARAM_STR);
+    $result -> bindParam(':image_path',$image_path,PDO::PARAM_STR);
+    $result -> bindParam(':prefecture',$prefecture,PDO::PARAM_INT);
+    $result -> bindParam(':foods', $food, PDO::PARAM_STR);
+    $result -> execute();
+}
---
+++ web/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <Meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>旬食ナビ 投稿</title>
+    <link rel="stylesheet" href="./com/import.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo center">旬食ナビ</a>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="mc">
+            <div class="syokuzai">
+                <h3>ローカル食材</h3>
+                <form action="./form.php" method="post" name="syokuzai">
+                    <div>
+                        <div class="input-field">
+                            <input placeholder="トマト" id="name" type="text" class="validate" name="name">
+                            <label for="name">食材名</label>
+                        </div>
+                    </div>
+                    <div>
+                        <label for="">食材画像</label>
+                        <div class="file-field input-field">
+                            <div class="btn">
+                                <span>File</span>
+                                <input type="file" name="image">
+                            </div>
+                            <div class="file-path-wrapper">
+                                <input class="file-path validate" type="text" name="image_path">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-field">
+                        <select name="prefecture" id="prefecture_1">
+                        </select>
+                        <label for="prefecture_1">地域名</label>
+                    </div>
+                    <button class="btn" name="submit_1">送信</button>
+                </form>
+            </div>
+            <div class="kondate">
+                <h3>献立</h3>
+                <form action="./form.php" method="post" name="kondate">
+                        <div class="input-field">
+                            <input placeholder="jigカレー" id="name" type="text" class="validate" name="name">
+                            <label for="name">献立名</label>
+                        </div>
+                        <div>
+                            <label for="">献立画像</label>
+                            <div class="file-field input-field">
+                                <div class="btn">
+                                    <span>File</span>
+                                    <input type="file" name="image">
+                                </div>
+                                <div class="file-path-wrapper">
+                                    <input class="file-path validate" type="text" name="image_path">
+                                </div>
+                            </div>
+                        </div>
+                    <div class="input-field">
+                        <select name="prefecture" id="prefecture_2">
+                        </select>
+                        <label for="prefecture_2">地域名</label>
+                    </div>
+                    <label for="">使用する食材</label>
+                    <p>
+                    jig課題のときと同じ検索方法
+                    データベースから食材一覧をとってきてjs配列に持たせてfilterで正規表現を使いながら検索する
+                    </p>
+                    <button class="btn" name="submit_2">送信</button>
+                </form>
+            </div>
+        </div>
+    </main>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="./index.js"></script>
+</body>
+</html>
