8ef7377efb9a06caa773c3b713a147d045f4f23e
takameron <takameron.info@gmail.com>
2019-08-28 16:28:16 +0900

    Update: DBのスキーマの更新

--- server/initdb/2_schema.sql
+++ server/initdb/2_schema.sql
@@ -1,34 +1,34 @@
-CREATE TABLE IF NOT EXISTS season_foods_navi.season_foods (
- id INT NOT NULL PRIMARY KEY,
- name VARCHAR(255),
- months VARCHAR(255)
-);
+CREATE TABLE IF NOT EXISTS season_foods_navi.foods (
+  `id` INT(11) NOT NULL,
+  `name` VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `base_food` INT(11) DEFAULT NULL,
+  `picture` VARCHAR(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `months` VARCHAR(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pref_id` INT(11) DEFAULT NULL,
+  `post` TINYINT(1) DEFAULT 1,
 
-
-CREATE TABLE IF NOT EXISTS season_foods_navi.prefecture (
- id INT NOT NULL PRIMARY KEY,
- name VARCHAR(255)
-);
-
+  PRIMARY KEY (`id`),
+  KEY `pref_id` (`pref_id`),
+  CONSTRAINT `foods_ibfk_1` FOREIGN KEY (`pref_id`) REFERENCES `prefecture` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS season_foods_navi.recipes (
- id INT NOT NULL PRIMARY KEY,
- name VARCHAR(255),
- picture VARCHAR(255),
- foods VARCHAR(255),
- pref_id INT,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `picture` VARCHAR(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `foods` LONGTEXT COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pref_id` INT(11) DEFAULT NULL,
+  `howto` LONGTEXT COLLATE utf8_unicode_ci DEFAULT NULL,
+  `post` TINYINT(1) DEFAULT 1,
 
- FOREIGN KEY (pref_id) REFERENCES prefecture (id)
-);
+  PRIMARY KEY (`id`),
+  KEY `pref_id` (`pref_id`),
+  CONSTRAINT `recipes_ibfk_1` FOREIGN KEY (`pref_id`) REFERENCES `prefecture` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+CREATE TABLE IF NOT EXISTS season_foods_navi.prefecture (
+  `id` INT(11) NOT NULL,
+  `name` VARCHAR(255) COLLATE utf8_unicode_ci DEFAULT NULL,
 
-CREATE TABLE IF NOT EXISTS season_foods_navi.local_foods (
- id INT NOT NULL PRIMARY KEY,
- name VARCHAR(255),
- picture VARCHAR(255),
- pref_id INT,
-
- FOREIGN KEY (pref_id) REFERENCES prefecture (id)
-);
-
-
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
