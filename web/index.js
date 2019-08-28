fa6a84a42ac654ed4f1500a951113db3ed7fca70
FukeKazki <kazkichi0906@gmail.com>
2019-08-28 11:16:12 +0900

    Update: Ajaxを使って県名を取得

--- web/index.js
+++ web/index.js
@@ -1,17 +1,32 @@
 'use strict'
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select')
-    var instances = M.FormSelect.init(elems, null)
+// document.addEventListener('DOMContentLoaded', function () {
+//     var elems = document.querySelectorAll('select')
+//     var instances = M.FormSelect.init(elems, null)
+// })
+$(document).ready(function () {
+    $('select').formSelect();
 });
 const prefecture_selectbox = document.querySelectorAll('select[name="prefecture"]')
-const prefecture_list = ['福岡', '福井', '北海道']
-let prefecture_dom = ''
-for(let i = 0; i < prefecture_selectbox.length; i++) {
-    prefecture_list.forEach((key, idx) => {
-        let option = document.createElement('option')
-        option.value = idx
-        option.text = key
-        if(!idx) option.selected = true 
-        prefecture_selectbox[i].appendChild(option)
+
+$(function () {
+    $.ajax({
+        type: 'get',
+        url: './getFoods.php',
+        dataType: 'json',
+    }).done(data => {
+        createOptions(data);
     })
+})
+const createOptions = (data) => {
+    console.log(data);
+    for (let i = 0; i < prefecture_selectbox.length; i++) {
+        data.forEach((key, idx) => {
+            let option = document.createElement('option')
+            option.value = key.id
+            option.text = key.name
+            if (!idx) option.selected = true
+            prefecture_selectbox[i].appendChild(option)
+        })
+    }
+    $('select').formSelect();
 }
