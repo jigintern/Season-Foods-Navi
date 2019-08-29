80b0a93d1883591c18a154f89efdbf3eafe2950f
FukeKazki <kazkichi0906@gmail.com>
2019-08-29 10:00:09 +0900

    Update; Ajaxでの取得の追加

--- web/index.js
+++ web/index.js
@@ -1,8 +1,4 @@
 'use strict'
-// document.addEventListener('DOMContentLoaded', function () {
-//     var elems = document.querySelectorAll('select')
-//     var instances = M.FormSelect.init(elems, null)
-// })
 $(document).ready(function () {
     $('select').formSelect();
 });
@@ -11,14 +7,20 @@ const prefecture_selectbox = document.querySelectorAll('select[name="prefecture"
 $(function () {
     $.ajax({
         type: 'get',
-        url: './getFoods.php',
+        url: './getPrefecture.php',
         dataType: 'json',
     }).done(data => {
         createOptions(data);
     })
+    $.ajax({
+        type: 'get',
+        url: './getFoods.php',
+        dataType: 'json',
+    }).done(data => {
+        createFoodsOption(data);
+    })
 })
 const createOptions = (data) => {
-    console.log(data);
     for (let i = 0; i < prefecture_selectbox.length; i++) {
         data.forEach((key, idx) => {
             let option = document.createElement('option')
@@ -28,5 +30,22 @@ const createOptions = (data) => {
             prefecture_selectbox[i].appendChild(option)
         })
     }
+    $('select').formSelect();
+}
+const createFoodsOption = (data) => {
+    const foods_selectbox = document.querySelector('select[name="foods[]"]')
+    data.forEach((key, idx) => {
+        let option = document.createElement('option')
+        option.value = key.id
+        option.text = key.name
+        foods_selectbox.appendChild(option)
+    })
+    const base_foods_selectbox = document.querySelector('select[name="base_food"]')
+    data.forEach((key, idx) => {
+        let option = document.createElement('option')
+        option.value = key.id
+        option.text = key.name
+        base_foods_selectbox.appendChild(option)
+    })
     $('select').formSelect();
 }
