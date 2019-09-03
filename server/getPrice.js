c56a89e1d65c824d9de6d40199943e10df124092
FukeKazki <kazkichi0906@gmail.com>
2019-09-03 15:49:17 +0900

    Update: 僕にはできないのでコメントを残してバトンタッチ

--- server/getPrice.js
+++ server/getPrice.js
@@ -1,6 +1,5 @@
 import mysql from 'mysql'
 const searchFoodPrice = (name) => {
-    // console.log('This is searchFoodPrice ' + name)
     return new Promise((resolve, reject) => {
         const fruits = []
         const seafoods = []
@@ -16,47 +15,99 @@ const searchFoodPrice = (name) => {
         }
         getAllFoodsPrice()
         const response = []
-        const highPrice_1 = []
-        const highPrice_2 = []
-        const mediumPrice_1 = []
-        const mediumPrice_2 = []
-        const object_1 = {}
-        const object_2 = {}
-        let cnt = 0
-        for (let i = 0; i < fruits.length; i++) {
-            fruits[i].filter((item, idx) => {
-                if (item.name === name) {
-                    if (cnt % 2 === 0) {
-                        object_1['place'] = item.place
-                        highPrice_1.push(item.HighPrice)
-                        mediumPrice_1.push(item.MediumPrice)
-                    } else {
-                        object_2['place'] = item.place
-                        highPrice_2.push(item.HighPrice)
-                        mediumPrice_2.push(item.MediumPrice)
+        const foods = [fruits, seafoods, vegetables]
+        /*
+        ~ foodsの中身 ~
+        foods = [
+            [
+                [
+                    {
+                        name: みかん,
+                        HighPrice: 200,
+                        MediumPeice: 190,
+                        place: 福井県
+                    }...×n個
+                ],
+                [] ...×8個
+            ],
+            [],
+            []
+        ]
+        */
+        for (let i = 0; i < foods.length; i++) {
+            foods[i].forEach((element, idx_1) => {
+                element.forEach((food, idx_2) => {
+                    if (food.name === name) {
+                        console.log(food)
                     }
-                    cnt++
-                }
+                })
             })
         }
-        object_1['HighPrice'] = highPrice_1
-        object_2['HighPrice'] = highPrice_2
-        object_1['MediumPrice'] = mediumPrice_1
-        object_2['MediumPrice'] = mediumPrice_2
-        response.push(object_1)
-        response.push(object_2)
-        resolve(response)
+        const dummy_response = [{
+                place: 'アメリカ合衆国',
+                HighPrice: [
+                    // ない月は '-' にする
+                    200, 200, 233, '-', '-', 259, 233, 250
+                ],
+                MediumPrice: [
+                    190, 190, 190, '-', '-', 190, 190, 200
+                ]
+            },
+            {
+                place: '福井県',
+                HighPrice: [
+                    // ない月は '-' にする
+                    200, 200, 233, '-', '-', 259, 233, 250
+                ],
+                MediumPrice: [
+                    190, 190, 190, '-', '-', 190, 190, 200
+                ]
+            }
+        ]
+        resolve(dummy_response)
+        // const response = []
+        // const highPrice_1 = []
+        // const highPrice_2 = []
+        // const mediumPrice_1 = []
+        // const mediumPrice_2 = []
+        // const object_1 = {}
+        // const object_2 = {}
+        // let cnt = 0
+
+        // for (let i = 0; i < fruits.length; i++) {
+        //     fruits[i].filter((item, idx) => {
+        //         if (item.name === name) {
+        //             if (cnt % 2 === 0) {
+        //                 object_1['place'] = item.place
+        //                 highPrice_1.push(item.HighPrice)
+        //                 mediumPrice_1.push(item.MediumPrice)
+        //             } else {
+        //                 object_2['place'] = item.place
+        //                 highPrice_2.push(item.HighPrice)
+        //                 mediumPrice_2.push(item.MediumPrice)
+        //             }
+        //             console.log('あった!! 月: ' + i + ' : ' + cnt)
+        //             cnt++
+        //         } else if (i === fruits.length - 1) {
+
+        //         }
+        //     })
+        // }
+        // object_1['HighPrice'] = highPrice_1
+        // object_2['HighPrice'] = highPrice_2
+        // object_1['MediumPrice'] = mediumPrice_1
+        // object_2['MediumPrice'] = mediumPrice_2
+        // response.push(object_1)
+        // response.push(object_2)
+        // resolve(response)
     })
 }
 const all = async (id) => {
     const name = await searchPrefecture(id)
-    // console.log('Prefecture が帰ってきたよ' + name)
     const prices = await searchFoodPrice(name)
-    // console.log('Priceが帰ってきたよ' + prices)
     return prices
 }
 const searchPrefecture = (id) => {
-    // console.log('This is searchPrefecture ' + id)
     const connection = mysql.createPool({
         host: 'mariadb',
         user: 'SeasonFoodsNavi',
@@ -75,3 +126,6 @@ const searchPrefecture = (id) => {
 module.exports = {
     searchFoodName: all
 }
+/**
+ * 値段データがないときは　"-"を入れるようにする
+ */
